be009383b1158388129e492ddaf220dea1a424b2
Maksym Hostroushko <makshostroyshko@gmail.com>
2023-08-28 09:37:40 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Creating 20 sample posts
+20.times do
+  post = Post.create!(
+    title: Faker::Book.title,
+    body: Faker::Lorem.paragraphs(number: 3).join("\n\n") # 3 paragraphs per post body
+  )
+
+  # Creating 3 comments for each post
+  3.times do
+    post.comments.create!(
+      body: Faker::Movies::HarryPotter.quote
+    )
+  end
+end
+
+puts "Seeded #{Post.count} posts and #{Comment.count} comments!"
