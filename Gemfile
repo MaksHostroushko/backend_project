b6de6e25b04c4a686a17fb4a4fb6f2907e827ead
Oleksandr Poltavets <sancho.ck@gmail.com>
2023-10-24 11:46:38 +0200

    add devise

--- Gemfile
+++ Gemfile
@@ -61,6 +61,8 @@ gem "sassc-rails"
 
 gem "sidekiq"
 
+gem "devise", "~> 4.9"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
 
   validates :first_name, :last_name, :email, presence: true
